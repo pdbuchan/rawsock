b6e5ffee53bf59e005a199ce210bde7f1ad5d6da
pdbuchan <pdbuchan@proton.me>
2026-05-06 19:08:28 -0400

    Update tcp4.c

--- ipv4/hdrincl/tcp4.c
+++ ipv4/hdrincl/tcp4.c
@@ -1,5 +1,5 @@
 
-/*  Copyright (C) 2011-2015  P.D. Buchan (pdbuchan@yahoo.com)
+/*  Copyright (C) 2011-2015  P.D. Buchan (pdbuchan@gmail.com)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
